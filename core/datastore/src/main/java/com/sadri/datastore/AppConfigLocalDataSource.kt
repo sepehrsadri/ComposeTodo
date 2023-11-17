167a8cdb0a947853b3631700212fd60a4c29e0dd
sepehr.sadri <sepehrsadri@gmail.com>
2023-11-17 23:33:14 +0330

    Refactor: change data store user saving and getting

--- core/datastore/src/main/java/com/sadri/datastore/AppConfigLocalDataSource.kt
+++ core/datastore/src/main/java/com/sadri/datastore/AppConfigLocalDataSource.kt
@@ -6,6 +6,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.sadri.datastore.di.AppConfigDataStoreQualifier
 import com.sadri.model.AppException
+import com.sadri.model.LocalUserEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -13,7 +14,34 @@ import javax.inject.Inject
 class AppConfigLocalDataSource @Inject constructor(
   @AppConfigDataStoreQualifier private val dataStore: DataStore<Preferences>
 ) {
-  suspend fun saveUserId(userId: String?) {
+
+  suspend fun saveUser(localUserEntity: LocalUserEntity) {
+    saveUserId(localUserEntity.id)
+    saveUsername(localUserEntity.username)
+  }
+
+  fun getLocalUser(): Flow<LocalUserEntity> {
+    return dataStore.data.map {
+      val username = it[USER_NAME_KEY]
+      val id = it[USER_ID_KEY]
+      LocalUserEntity(
+        id = id,
+        username = username
+      )
+    }
+  }
+
+  private suspend fun saveUsername(userName: String?) {
+    dataStore.edit {
+      if (userName == null) {
+        it.remove(USER_NAME_KEY)
+      } else {
+        it[USER_NAME_KEY] = userName
+      }
+    }
+  }
+
+  private suspend fun saveUserId(userId: String?) {
     dataStore.edit {
       if (userId == null) {
         it.remove(USER_ID_KEY)
@@ -23,18 +51,8 @@ class AppConfigLocalDataSource @Inject constructor(
     }
   }
 
-  fun getUserId(): Flow<Result<String>> {
-    return dataStore.data.map {
-      val id = it[USER_ID_KEY]
-      if (id != null) {
-        Result.success(id)
-      } else {
-        Result.failure(AppException.EmptyUserId)
-      }
-    }
-  }
-
   companion object {
     private val USER_ID_KEY = stringPreferencesKey("userIdKey")
+    private val USER_NAME_KEY = stringPreferencesKey("userNameKey")
   }
 }
--- core/domain/src/main/java/com/sadri/domain/SaveLocalUserUseCase.kt
+++ core/domain/src/main/java/com/sadri/domain/SaveLocalUserUseCase.kt
@@ -1,11 +1,12 @@
 package com.sadri.domain
 
 import com.sadri.data.repository.AppConfigRepository
+import com.sadri.model.LocalUserEntity
 import javax.inject.Inject
 
-class SaveUserIdUseCase @Inject constructor(
+class SaveLocalUserUseCase @Inject constructor(
   private val appConfigRepository: AppConfigRepository
 ) {
-  suspend operator fun invoke(userId: String?) =
-    appConfigRepository.saveUserId(userId)
+  suspend operator fun invoke(localUserEntity: LocalUserEntity) =
+    appConfigRepository.saveUser(localUserEntity)
 }
